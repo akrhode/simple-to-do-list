45e45a24c0c8271a224cdd6e579ffd37b2aa1c64
akrhode <a.rhode@web.de>
2023-01-11 23:44:36 +0100

    Add colors

--- skript.js
+++ skript.js
@@ -10,6 +10,7 @@ addToDoButton.addEventListener("click", function () {
   inputField.value = "";
   paragraph.addEventListener("click", function () {
     paragraph.style.textDecoration = "line-through";
+    paragraph.style.color = "red";
   });
   paragraph.addEventListener("dblclick", function () {
     toDoContainer.removeChild(paragraph);
